3212bcbbc2f980f8b63ccdc08ec368bbb8e09358
renangomeslino <00001094350722sp@al.educacao.sp.gov.br>
2024-10-24 17:14:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'A capital do Brasil é Brasília.'
+)
+
+criaCartao(
+    'Física',
+    'Qual é a fórmula da força?',
+    'A fórmula da força é F = m * a, onde F é a força, m é a massa e a é a aceleração.'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma reação química?',
+    'Uma reação química é um processo em que substâncias se transformam em novas substâncias com propriedades diferentes.'
+)
+
+criaCartao(
+    'Literatura',
+    'Quem escreveu "Dom Casmurro"?',
+    'O livro "Dom Casmurro" foi escrito por Machado de Assis.'
+)
